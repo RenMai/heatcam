d9783da2d401c5c6944a9179b12027ceac6dcf03
vadimzubchenko <vadim.zubchenko@metropolia.fi>
2020-09-20 21:18:54 +0300

    user temperature layout

---
+++ app/src/main/java/com/example/heatcam/User_result.java
@@ -0,0 +1,13 @@
+package com.example.heatcam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class User_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_result);
+    }
+}
