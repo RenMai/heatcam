6de6f38098dc575a46ae0acda7afa34936e86ad7
eerotuu <eerotuu@metropolia.fi>
2020-09-30 19:08:07 +0300

    added start layout draft

---
+++ app/src/main/java/com/example/heatcam/MenuFragment.java
@@ -0,0 +1,34 @@
+package com.example.heatcam;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.menu_layout, container, false);
+
+        Fragment f = new CameraTestFragment();
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .add(R.id.menu_dev_fragment, f, "default").commit();
+
+        view.findViewById(R.id.menu_start_auto_button).setOnClickListener(v -> {
+            Fragment fragment = new MeasurementStartFragment();
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(R.animator.slide_in_left, R.animator.slide_in_right, 0, 0)
+                    .replace(R.id.fragmentCamera, fragment, "measure_start")
+                    .commit();
+        });
+
+        return view;
+    }
+}
