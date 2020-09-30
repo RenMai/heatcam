550b48f0e585dd0ece31fa6883466605c9d6a7b8
Vadim Zubchenko <vadim.zubchenko@metropolia.fi>
2020-10-01 01:23:54 +0300

    new changes in User_result.java, it works well

--- app/src/main/java/com/example/heatcam/User_result.java
+++ app/src/main/java/com/example/heatcam/User_result.java
@@ -8,7 +8,6 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -17,12 +16,13 @@ import android.widget.TextView;
 public class User_result extends AppCompatActivity implements CameraListener {
 
     // the value could be used of user temperature when userTemp is 100/real 39C etc.
-    private double userTemp = 0;
+    private double userTemp = 0, correcTemp = 0;
     double temp = 0;
 
-    private Button buttonStart, buttonStart2, buttonStart3;
+    private Button buttonStart3;
     private TextView text, text2;
     private boolean ready = false;
+
     private int laskuri = 0;
     ProgressBar vProgressBar;
     SerialPortModel serialPortModel;
@@ -37,46 +37,20 @@ public class User_result extends AppCompatActivity implements CameraListener {
         animationDrawable.setEnterFadeDuration(2000);
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
-        text = findViewById(R.id.textView);
+
+
         //new asyncTaskUpdateProgress().execute();
         serialPortModel = SerialPortModel.getInstance();
         serialPortModel.setCamListener(this);
-
-//        buttonStart = findViewById(R.id.start);
-//        buttonStart2 = findViewById(R.id.start2);
         buttonStart3 = findViewById(R.id.start3);
         text = findViewById(R.id.textView);
         text2 = findViewById(R.id.textView2);
-        text2.setText("THERMAL CAMERA");
+        text2.setText(R.string.otsikko);
         vProgressBar = findViewById(R.id.vprogressbar3);
 
-
-//        buttonStart.setOnClickListener(v -> {
-//            userTemp = 37.8;
-//            // TODO Auto-generated method stub
-//            buttonStart.setClickable(false);
-//            new asyncTaskUpdateProgress().execute();
-//            text = findViewById(R.id.textView);
-//            text.setText(R.string.msgHightTmprt);
-//        });
-//        buttonStart2.setOnClickListener(v -> {
-//            userTemp = 35.5;
-//            // TODO Auto-generated method stub
-//            buttonStart2.setClickable(false);
-//            new asyncTaskUpdateProgress().execute();
-//            text.setText(R.string.msgNormTmprt);
-//        });
         buttonStart3.setOnClickListener(v -> {
-            //buttonStart3.setClickable(false);
             ready = true;
-            /*
-            userTemp = 30;
-            // TODO Auto-generated method stub
-            buttonStart3.setClickable(false);
-            new asyncTaskUpdateProgress().execute();
-            text = findViewById(R.id.textView);
-            text.setText(R.string.msgLowTmprt);
-            */
+            userTemp = 0;
         });
     }
 
@@ -106,25 +80,24 @@ public class User_result extends AppCompatActivity implements CameraListener {
             if (laskuri < 100) {
                 if (max > userTemp) {
                     userTemp = max;
-                }
+            }
                 laskuri++;
             } else {
                 ready = false;
                 laskuri = 0;
-
+                correcTemp = userTemp + 2.5;
                 new asyncTaskUpdateProgress().execute();
 
-                if (75 >= userTemp && userTemp >= 27) {
+                if (37.5 >= correcTemp && correcTemp >= 35.5) {
                     text.setText(R.string.msgNormTmprt);
-                } else if (userTemp > 75) {
+
+                } else if (correcTemp > 37.5) {
                     text.setText(R.string.msgHightTmprt);
                 } else {
                     text.setText(R.string.msgLowTmprt);
                 }
-
             }
         }
-
     }
 
     @Override
@@ -144,7 +117,7 @@ public class User_result extends AppCompatActivity implements CameraListener {
         @Override
         protected void onPostExecute(Void result) {
             // TODO Auto-generated method stub
-            // buttonStart3.setClickable(true);
+           // buttonStart3.setClickable(true);
 
 
         }
@@ -164,17 +137,19 @@ public class User_result extends AppCompatActivity implements CameraListener {
         @Override
         protected Void doInBackground(Void... arg0) {
             // TODO Auto-generated method stub
+            double tmin = 29, tmax = 39;
 
-            temp = (userTemp - 27)/(41 - 27)*100;
+            text2.setText(Double.toString(correcTemp));
+            temp = (userTemp - tmin)/(tmax - tmin)*100;
             while (progress < temp) {
                 progress++;
                 publishProgress(progress);
                 SystemClock.sleep(1);
-
             }
             return null;
 
         }
+
 
 
 
