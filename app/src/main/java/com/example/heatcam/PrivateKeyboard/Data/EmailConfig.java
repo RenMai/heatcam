fad873ad168a98e4eda4e6bcfb3d73d90b6b4850
eerotuu <eerotuu@metropolia.fi>
2020-12-01 20:00:46 +0200

    added private keyboard files to project

---
+++ app/src/main/java/com/example/heatcam/PrivateKeyboard/Data/EmailConfig.java
@@ -0,0 +1,6 @@
+package com.example.heatcam.PrivateKeyboard.Data;
+
+public class EmailConfig {
+    public static final String EMAIL ="maithieunhan@gmail.com";
+    public static final String PASSWORD ="tiowinpbxqsckjqh";
+}
